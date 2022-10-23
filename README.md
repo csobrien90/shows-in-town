b2203d66a851077a04a2cadbb51e5daf62c0983c
Chad OBrien <obrien.music@gmail.com>
2022-10-22 20:03:10 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# shows-in-town
+# Shows In Town
+*A web app to compile and display information for live music shows in Louisville, Kentucky*
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return (
+			<main>CONTENT</main>
+		)
+	}
+}
+
+export default App;
