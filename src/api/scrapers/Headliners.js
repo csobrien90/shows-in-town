adaa71e43b483fe302d2da36db22432859429add
Chad OBrien <obrien.music@gmail.com>
2022-10-23 19:06:25 -0400

    add epoch to Headliners scraper

--- src/api/scrapers/Headliners.js
+++ src/api/scrapers/Headliners.js
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
 
-export async function scrape() {
+export async function scrapeHeadliners() {
 	// Start and configure puppeteer
 	const browser = await puppeteer.launch({})
 	const page = await browser.newPage()
@@ -40,12 +40,57 @@ export async function scrape() {
 		} else {
 			desc = desc.replace('|', '')
 		}
+
+		// Generate epoch
+		let dateArr = date.split(' ');
+		let timeArr = time.split(' ');
+
+		let now = new Date();
+		let currentMonth = now.getMonth();
+		let currentDay = now.getDate();
+
+		let monthLookup = {
+			JAN: '01',
+			FEB: '02',
+			MAR: '03',
+			APR: '04',
+			MAY: '05',
+			JUN: '06',
+			JUL: '07',
+			AUG: '08',
+			SEP: '09',
+			OCT: '10',
+			NOV: '11',
+			DEC: '12'
+		}
+		
+	
+		let month = monthLookup[dateArr[1]];
+		let day = dateArr[2];
+
+		let year;
+		if (+month < currentMonth || ((+month === currentMonth) && (+day < currentDay))) {
+			year = now.getFullYear() + 1;
+		} else {
+			year = now.getFullYear();
+		}
+	
+		let startTime = timeArr[2];
+		let isAM = timeArr[2].includes('am');
+		let hourNumber = timeArr[2].substring(0, timeArr[2].length - 2).split(':')[0]; 
+		let hour = isAM ? hourNumber : +hourNumber + 12;
+		let formattedHour = ('0' + hour).slice(-2);
+		let minutes = startTime.includes(':') ? startTime.slice(startTime.indexOf(':'), startTime.indexOf(':') + 2) : '00';
+
+		let parsableDate = `${year}-${month}-${day}T${formattedHour}:${minutes}:00`;
+		let epoch = Date.parse(parsableDate);
 		
 		// Tidy up data and push to events array
 		events.push({
 			title: title.trim(),
 			address: 'Headliners Music Hall - 1386 Lexington Rd. Louisville, KY',
 			time: dateTime,
+			epoch,
 			desc,
 			link
 		});
@@ -59,4 +104,4 @@ export async function scrape() {
 
 }
 
-console.log(await scrape())
+console.log(await scrapeHeadliners())
