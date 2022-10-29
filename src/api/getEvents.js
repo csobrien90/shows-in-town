e5af6f5660143b0aaa6e17c7c3263f91153d5f61
Chad OBrien <obrien.music@gmail.com>
2022-10-28 23:34:43 -0400

    add Stevie Rays scraper

--- src/api/getEvents.js
+++ src/api/getEvents.js
@@ -1,20 +1,24 @@
 import { scrapeHeadliners } from "./scrapers/Headliners.js";
 import { scrapeLouisvilleJazzSociety } from "./scrapers/LouisvilleJazzSociety.js";
 import { scrapeBlackJockeysLounge } from "./scrapers/BlackJockeysLounge.js";
+import { scrapeStevieRays } from "./scrapers/StevieRays.js";
 
 export async function getEvents() {
 	const [headliners,
 		jazzSociety,
-		blackJockeysLounge
+		blackJockeysLounge,
+		stevieRays
 	] = await Promise.all([
 		scrapeHeadliners(),
 		scrapeLouisvilleJazzSociety(),
-		scrapeBlackJockeysLounge()
+		scrapeBlackJockeysLounge(),
+		scrapeStevieRays()
 	])
 
 	return [
 		...headliners,
 		...jazzSociety,
-		...blackJockeysLounge
+		...blackJockeysLounge,
+		...stevieRays
 	];
 }
---
+++ src/api/scrapers/StevieRays.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+export async function scrapeStevieRays() {
+	// Define initial request details
+	const baseUrl = 'https://www.stevieraysbluesbar.com/api/open/'
+	const route = 'GetItemsByMonth'
+	const collectionId = '524eeefbe4b05e9b207a6bea'
+
+	// Create query params for this month and the next two months
+	const now = new Date()
+	const nextMonthDateTime = new Date(now.getFullYear(), now.getMonth()+1, 1)
+	const twoMonthsFromNowDateTime = new Date(nextMonthDateTime.getFullYear(), nextMonthDateTime.getMonth()+1, 1)
+	
+	let thisMonthParam = `${now.getMonth() + 1}-${now.getFullYear()}`
+	let nextMonthParam = `${nextMonthDateTime.getMonth() + 1}-${nextMonthDateTime.getFullYear()}`
+	let twoMonthsFromNowParam = `${twoMonthsFromNowDateTime.getMonth() + 1}-${twoMonthsFromNowDateTime.getFullYear()}`
+	
+	// Get events for those months and compile into rawEvents
+	const [thisMonth, nextMonth, twoMonthsFromNow] = await Promise.all([
+		axios.get(`${baseUrl}${route}?collectionId=${collectionId}&month=${thisMonthParam}`),
+		axios.get(`${baseUrl}${route}?collectionId=${collectionId}&month=${nextMonthParam}`),
+		axios.get(`${baseUrl}${route}?collectionId=${collectionId}&month=${twoMonthsFromNowParam}`),
+	])
+
+	const rawEvents = [
+		...thisMonth.data,
+		...nextMonth.data,
+		...twoMonthsFromNow.data
+	]
+	
+	// Iterate over elements and populate events array
+	let events = [];
+	for (let e of rawEvents) {
+		const epoch = e.startDate
+		if (epoch < Date.parse(now) - 10800000) continue
+
+		const title = e.title
+		if (title.includes('AVAILABLE FOR SPECIAL EVENTS')) continue
+		
+		let firstTitle = title.split('(').shift().trim()
+		let secondaryTitle = null
+		if (title.includes('-----')) secondaryTitle = title.split('-----')[1].split('(')[0].trim()
+		let shortenedTitle = secondaryTitle ?  `${firstTitle} and ${secondaryTitle}` : firstTitle
+
+		// Tidy up data and push to events array
+		events.push({
+			title: shortenedTitle,
+			address: `${e.location.addressTitle} - ${e.location.addressLine1} ${e.location.addressLine2}`,
+			time: `${new Date(e.startDate).toLocaleTimeString()} - ${new Date(e.endDate).toLocaleTimeString()}`,
+			epoch,
+			desc: title,
+			link: 'https://www.stevieraysbluesbar.com' + e.fullUrl
+		});
+	}
+
+	return events
+
+}
+
+// console.log(await scrapeStevieRays())
