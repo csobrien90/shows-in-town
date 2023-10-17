c7bdeac1192a2a96b95571779523f23c6b2f5428
Chad OBrien <obrien.music@gmail.com>
2023-10-17 10:54:11 -0400

    prevent circular json stringify bug

--- src/components/DataCollector.js
+++ src/components/DataCollector.js
@@ -23,19 +23,30 @@ const DataCollector = ({children}) => {
 	// Record all user interactions with the page
 	const [events, setEvents] = useState([])
 
-	const recordEvent = (e) => {
-		const event = {
-			type: e.type,
-			target: e.target,
-			time: Date.now() - timeOfPageLoad
+	useEffect(() => {
+		const recordEvent = (e) => {
+			const event = {
+				type: e.type,
+				time: Date.now() - timeOfPageLoad
+			}
+	
+			if (e.type === 'click') {
+				event.target = {
+					tagName: e.target.tagName,
+					innerText: e.target.innerText,
+					id: e.target.id,
+					classList: [...e.target.classList]
+				}
+			} else if (e.type === 'scroll') {
+				event.scrollX = window.scrollX
+				event.scrollY = window.scrollY
+			}
+	
+			setEvents(prevEvents => {
+				return [...prevEvents, event]
+			})
 		}
 
-		setEvents(prevEvents => {
-			return [...prevEvents, event]
-		})
-	}
-	
-	useEffect(() => {
 		['click', 'scroll'].forEach(eventType => {
 			window.addEventListener(eventType, (e) => {
 				recordEvent(e)
@@ -43,39 +54,40 @@ const DataCollector = ({children}) => {
 		})
 	}, [])
 
-	// Prepare user data object to send to db
-	const makeUserDataObject = () => {
-		// Calculate time on page
-		const timeOnPage = Date.now() - timeOfPageLoad
-
-		// Return user data object
-		return {
-			userAgent,
-			timeOnPage,
-			ipData,
-			events
-		}
-	}
-
-	// Send data to db when user leaves the page
-	const sendData = () => {
-		// Make user data object
-		const userData = makeUserDataObject()
-
-		// Send data to db
-		fetch('https://z32mbfotsov62vonmya6xsnyz40lydhm.lambda-url.us-east-2.on.aws/', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(userData),
-			keepalive: true
-		})
-	}
-
 	// Add event listener to send data when user leaves the page
 	useEffect(() => {
+		// Prepare user data object to send to db
+		const makeUserDataObject = () => {
+			// Calculate time on page
+			const timeOnPage = Date.now() - timeOfPageLoad
+	
+			// Return user data object
+			return {
+				userAgent,
+				timeOnPage,
+				ipData,
+				events
+			}
+		}
+
+		// Send data to db when user leaves the page
+		const sendData = () => {
+			// Make user data object
+			const userData = makeUserDataObject()
+	
+			// Send data to db
+			fetch('https://z32mbfotsov62vonmya6xsnyz40lydhm.lambda-url.us-east-2.on.aws/', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(userData),
+				keepalive: true
+			})
+		}
+
 		window.addEventListener('beforeunload', sendData)
+
 		return () => {
 			window.removeEventListener('beforeunload', sendData)
 		}
